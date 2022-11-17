4f2d2c18d0fb6011646024ac8ac4263a91d0633d
ankita140694sakolkar <manakariankita@gmail.com>
2022-11-17 10:26:01 +0530

    first commit

---
+++ controller/authController.js
@@ -0,0 +1,45 @@
+const {StatusCodes } = require('http-status-codes')
+
+const authController ={
+    register: async(req,res) =>{
+        try{
+            res.json({ msg :"register"})   
+        
+        }catch(err){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err.message})
+        }
+    },
+    login: async(req,res) =>{
+        try{
+            res.json({ msg :"login"})   
+        
+        }catch(err){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err.message})
+        }
+    },
+    logout: async(req,res) =>{
+        try{
+            res.json({ msg :"logout"})   
+        
+        }catch(err){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err.message})
+        }
+    },
+    refreshToken: async(req,res) =>{
+        try{
+            res.json({ msg :"refresh token"})   
+        
+        }catch(err){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err.message})
+        }
+    },
+    resetPassword: async(req,res) =>{
+        try{
+          res.json({ msg :"reset password"})   
+        }catch(err){
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err.message})
+        }
+    },
+}
+
+module.exports = authController
